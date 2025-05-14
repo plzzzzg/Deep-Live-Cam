6f25e16a224bd7cd113aab5c53819bb2f4a18b25
guopeilun <guopl@flatincbr.com>
2025-05-14 11:34:51 +0800

    fix

--- modules/face_analyser.py
+++ modules/face_analyser.py
@@ -19,7 +19,7 @@ def get_face_analyser() -> Any:
     global FACE_ANALYSER
 
     if FACE_ANALYSER is None:
-        FACE_ANALYSER = insightface.app.FaceAnalysis(name='buffalo_l', providers=modules.globals.execution_providers, provider_options=modules.globals.providers_options)
+        FACE_ANALYSER = insightface.app.FaceAnalysis(name='buffalo_l', providers=modules.globals.execution_providers, provider_options=modules.globals.provider_options)
         FACE_ANALYSER.prepare(ctx_id=0, det_size=(640, 640))
     return FACE_ANALYSER
 
